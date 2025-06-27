43ac97cc0d031be72b02c77c09d0275c987d161b
harpreetdev01 <harpreetdev01@gmail.com>
2025-06-27 16:59:35 -0400

    completed assignment

--- age-checker.js
+++ age-checker.js
@@ -1,7 +1,8 @@
 // DOM Elements
-const ageField       = document.querySelector('#age_input');
-const submitBtn      = document.querySelector('#submit_age');
-const messageDisplay = document.querySelector('#input_message');
+const ageInputField       = document.querySelector('#age_input');
+const submitBtn           = document.querySelector('#submit_age');
+const messageDisplay      = document.querySelector('#input_message');
+const resetBtn            = document.querySelector('#reset_btn');
 
 // Global variables
 let userMessage = null;
@@ -9,40 +10,95 @@ const AGE_THRESHOLD = 18;
 
 // Message to display to user
 let ageMessages = {
-    underage: "You are a minor",
+    child: "You are a child",
+    teen: "You are a minor",
     adult: "You are an adult",
+    senior: "You are a senior",
     invalid: "Invalid entry, number(s) only",
-    empty: "Input can not be empty"
+    empty: "Input can not be empty",
+    outOfRange: "Input out of range"
 };
 
 // Utility function - display messages
 function displayMessage(messageText){
     messageDisplay.textContent = messageText;
-    ageField.value = "";
+    ageInputField.value = "";
 }
 
 // Main handler to evaluate input
 function evaluateAgeInput(){
-    const input = ageField.value.trim();
+    const input = ageInputField.value.trim();
+    const convertedInputToNumber = Number(input);
 
+    console.log(convertedInputToNumber);
+
+    // Check for empty input
     if(input === ""){
         displayMessage(ageMessages.empty);
-    } else if (isNaN(input)){
+
+        // check for not a number input
+    } else if(isNaN(convertedInputToNumber)){
         displayMessage(ageMessages.invalid);
-    } else if (Number(input) < AGE_THRESHOLD){
-       displayMessage(ageMessages.underage);
-    } else {
-        displayMessage(ageMessages.adult);
+    } else { // Success
+        // if(convertedInputToNumber >=5 && convertedInputToNumber <= 12){
+        //     displayMessage(ageMessages.child);
+        // } else if(convertedInputToNumber >= 13 && convertedInputToNumber <= 19){
+        //     displayMessage(ageMessages.teen);
+        // } else if(convertedInputToNumber >= 20 && convertedInputToNumber <= 64) {
+        //     displayMessage(ageMessages.adult);
+        // } else {
+        //     displayMessage(ageMessages.senior);
+        // }
+
+        // Test with Switch
+        switch(true){
+            case(convertedInputToNumber >= 5 && convertedInputToNumber <= 12):
+               displayMessage(ageMessages.child);
+               break;
+            case(convertedInputToNumber >= 13 && convertedInputToNumber <= 19):
+               displayMessage(ageMessages.teen);
+               break;
+            case(convertedInputToNumber >= 20 && convertedInputToNumber <= 64):
+                displayMessage(ageMessages.adult);
+                break;
+            case(convertedInputToNumber >= 65 && convertedInputToNumber <= 110):
+                displayMessage(ageMessages.senior);
+                break;
+            default:
+                displayMessage(ageMessages.outOfRange);
+        }
+
     }
+    
+}
+
+
+function resetInputField(inputEl){
+    inputEl.value = "";
+
+}
+
+function resetMessage(messageEl){
+    messageEl.textContent = "";
+}
+
+function reset(){
+
+    // Reset input field
+    resetInputField(ageInputField);
+
+    // Reset message
+    resetMessage(messageDisplay);
 }
 
 // Event listeners - submit button and enter keyword press
 submitBtn.addEventListener('click', evaluateAgeInput);
-ageField.addEventListener('keypress', (e) => {
+ageInputField.addEventListener('keypress', (e) => {
     if(e.code == 'Enter') {
         evaluateAgeInput();
     }    
 });    
+resetBtn.addEventListener('click', reset)
 
 
 
